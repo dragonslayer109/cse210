4177ab6dc1b024c2c47188cd625b830a1eae07f0
dragonslayer109 <raydonvetten@gmail.com>
2022-01-12 10:54:23 +0200

    Update tic-tac-toe_V01.py
    Logic added

--- Week 2/tic-tac-toe_V01.py
+++ Week 2/tic-tac-toe_V01.py
@@ -1,6 +1,22 @@
 
 def main():
+    player1 = "X"
+    player2 = "O"
+    player1_ticket = 1
+    player2_ticket = 0
     board = create_board()
+    while not (winner(board) or draw(board)):
+        display_board(board)
+        if player1_ticket == 1:
+            player = player1
+            player1_ticket = 0
+            prompt_move(player, board)
+            player2_ticket = 1
+        if player2_ticket == 1:
+            player = player2
+            player2_ticket = 0
+            prompt_move(player, board)
+            player1_ticket = 1            
     display_board(board)
     print("Good game. Thanks for playing!") 
 
@@ -19,6 +35,15 @@ def display_board(board):
     print('-+-+-')
     print(f"{board[6]}|{board[7]}|{board[8]}")
     print()
+
+def draw():
+    pass
+
+def winner():
+    pass
+
+def prompt_move(player, board):
+    pass
 
 if __name__ == "__main__":
     main()
