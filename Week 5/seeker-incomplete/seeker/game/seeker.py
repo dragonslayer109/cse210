82aa49714133ed935e2445434477b8d1a8079f0a
dragonslayer109 <raydonvetten@gmail.com>
2022-02-01 11:16:45 +0200

    Update seeker.py
    feat- class and methods added
    
    seeker class created. get and move location added as methods to the class.

--- Week 5/seeker-incomplete/seeker/game/seeker.py
+++ Week 5/seeker-incomplete/seeker/game/seeker.py
@@ -1,7 +1,7 @@
 # TODO: Implement the Seeker class as follows...
 
 # 1) Add the class declaration. Use the following class comment.
-
+class Seeker:
     """The person looking for the Hider. 
     
     The responsibility of a Seeker is to keep track of its location and distance travelled.
@@ -11,13 +11,15 @@
     """
 
 # 2) Create the class constructor. Use the following method comment.
+    def __init__(self):
         """Constructs a new Seeker.
 
         Args:
             self (Seeker): An instance of Seeker.
         """
-       
+  
 # 3) Create the get_location(self) method. Use the following method comment.
+    def get_location(self):
         """Gets the current location.
         
         Returns:
@@ -25,6 +27,7 @@
         """
         
 # 4) Create the move_location(self, location) method. Use the following method comment.
+    def move_location(self, location):
         """Moves to the given location.
 
         Args:
