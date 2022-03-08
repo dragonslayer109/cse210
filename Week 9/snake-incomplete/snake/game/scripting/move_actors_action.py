cf82f89d15ee9a4e863cae12f32eeac2519a9995
dragonslayer109 <raydonvetten@gmail.com>
2022-03-08 09:26:05 +0200

    Update move_actors_action.py
    feat- movement action added
    
    action to move actors(objects) added to the class MoveActorsAction.

--- Week 9/snake-incomplete/snake/game/scripting/move_actors_action.py
+++ Week 9/snake-incomplete/snake/game/scripting/move_actors_action.py
@@ -7,3 +7,17 @@ from game.scripting.action import Action
 # 1) get all the actors from the cast
 # 2) loop through the actors
 # 3) call the move_next() method on each actor
+class MoveActorsAction(Action):
+    """
+    Moves objects (this is done with the action script)
+    """
+    def __init__(self):
+        self.objects = []
+
+    def execute(self, cast, script):
+        """
+        get list of objects and ittirate through them
+        """
+        self.objects = cast.get_all_actors()
+        for object in self.objects:
+            object.move_next()
